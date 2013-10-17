72b1f1cb28b9a9ed57c85abfc382dfd39ead8573
JoaquinVarela <JoaquinVarela@90754bc4-90f5-4337-aa5b-9bc6757740f8>
2013-10-17 15:17:24 +0000

    Se agrega información de restaurantes

--- src/dominio/Informacion.java
+++ src/dominio/Informacion.java
@@ -21,14 +21,14 @@ public class Informacion {
                 + "excelente, con mar profundo y fuerte, arena abundante y protegida. "
                 + "En verano es sumamente concurrida a la tarde, después de las tres."
                 + "Siempre hay eventos, música, gimnasia aeróbica al atardecer, práctica de surf y jetsky.";        
-        Actividad bikini = new Actividad("Playa", "Playa Bikini",descripcionBikini, null, imagenBikini.getImage());     
+        Actividad bikini = new Actividad("Playa", "Bikini",descripcionBikini, null, imagenBikini.getImage());     
         listaAct.add(bikini);  
         
         
         
         String descripcionJI = "Las playas de Jose Ignacio se encuentran a 30 km al este de la península de Punta del Este "
                 + "posee playa mansa y brava. La gente que visita estas playas disfrutan de la tranquilidad del balneario.";        
-        Actividad playaJI = new Actividad("Playa", "Playa Jose Ignacio",descripcionJI, null, null); 
+        Actividad playaJI = new Actividad("Playa", "Jose Ignacio",descripcionJI, null, null); 
         listaAct.add(playaJI);
         
         
@@ -49,19 +49,55 @@ public class Informacion {
         String desPlayaMontoya = "La playa de montoya es ancha, de arena gruesa y mar con olas de considerable fuerza y altura. "
                 + "Por esta razon es que se lleva a cabo una de las etapas del campeonato mundial del surf. Es muy recomendada "
                 + "entre la gente joven";        
-        Actividad playaMontoya = new Actividad("Playa", "Playa de la Barra",desPlayaMontoya, null, null);
+        Actividad playaMontoya = new Actividad("Playa", "Montoya",desPlayaMontoya, null, null);
         listaAct.add(playaMontoya);
         
         
         String desPlayaPostaDelCangrejo = "La Posta de Cangrejo posee arena fina y humeda la cual ayuda a que la temperatura no "
                 + "sea demasiado elevada en verano. El hecho de que la arena sea humeda facilita los juegos con pelota.";        
-        Actividad playaPostaDelCangrejo = new Actividad("Playa", "Playa de la Barra",desPlayaPostaDelCangrejo, null, null);
+        Actividad playaPostaDelCangrejo = new Actividad("Playa", "La Posta de Cangrejo",desPlayaPostaDelCangrejo, null, null);
         listaAct.add(playaPostaDelCangrejo);
         
         
         
+        String desElChorro = "Playa vecina al balneario de Manantiales a 20 minutos de la península de Punta del Este. "
+                + "se considera una playa agresiva por el nivel de la corriente marina";        
+        Actividad playaElChorro = new Actividad("Playa", "El Chorro",desElChorro, null, null);
+        listaAct.add(playaElChorro);       
+    }
+    
+    
+    public void cargarRestaurantes(ArrayList<Actividad> listaAct) {
+        String desVirazon = "Ubicado a pasos del puerto de Punta del Este, sobre la mansa se disfruta de la vista a "
+                + "la Playa Mansa con una variedad amplia de platos: mariscos, pescados y carnes, arroces, tragos. "
+                + "Mirar también 'Lo De Tere' y 'Guappa'";        
+        Actividad virazon = new Actividad("Restaurant", "Virazón",desVirazon, null, null);
+        listaAct.add(virazon); 
+        
+        String desTere = "Ubicado a pasos del puerto de Punta del Este, sobre la mansa se disfruta de la vista a "
+                + "la Playa Mansa con una variedad amplia de platos: mariscos, pescados y carnes, arroces, tragos. "
+                + "Mirar también 'Virazon' y 'Guappa'";        
+        Actividad loDeTere = new Actividad("Restaurant", "Lo de Tere",desTere, null, null);
+        listaAct.add(loDeTere); 
+        
+        String desGuappa = "Ubicado a pasos del puerto de Punta del Este, sobre la mansa se disfruta de la vista a "
+                + "la Playa Mansa con una variedad amplia de platos: mariscos, pescados y carnes, arroces, tragos. "
+                + "Mirar también 'Lo De Tere' y 'Virazon'";       
+        Actividad guappa = new Actividad("Restaurant", "Guappa",desGuappa, null, null);
+        listaAct.add(guappa); 
         
         
+        String desLau = "Conocida popularmente como la torre de los waffles es un hotel que ofrece excelente servicio "
+                + "de salón de té muy lujoso. Especial para ir con pareja.";       
+        Actividad lau = new Actividad("Restaurant", "L'Auberge",desLau, null, null);
+        listaAct.add(lau); 
+        
+        
+        String desArtico = "Reconocido restaurant para comer ricos mariscos y pescados al borde del agua a la hora del "
+                + "almuerzo. Esta ubicado en el puerto de Punta del Este.";       
+        Actividad artico = new Actividad("Restaurant", "Artico",desArtico, null, null);
+        listaAct.add(artico);
+            
     }
     
 }
