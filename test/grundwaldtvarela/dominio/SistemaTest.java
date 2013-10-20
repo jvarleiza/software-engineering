2a52b41485590760613ac7287fcc90fdcc790a0b
palmera92 <palmera92@90754bc4-90f5-4337-aa5b-9bc6757740f8>
2013-10-20 21:52:56 +0000

    cambios menores

--- test/grundwaldtvarela/dominio/SistemaTest.java
+++ test/grundwaldtvarela/dominio/SistemaTest.java
@@ -132,7 +132,7 @@ public class SistemaTest {
                 expResult.add(a);
         }
         
-        ArrayList result = instance.getBares();
+        ArrayList result = instance.getRestaurantes();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
